1e438300a24b96cc9d0900cecfc69c965d87e102
Dennis Adaku <dennisadaku@ymail.com>
2024-05-27 12:19:47 +0000

    fix: app.js - typo in 'querySelectorAll'

--- 0x01-rock-paper-scissors/app.js
+++ 0x01-rock-paper-scissors/app.js
@@ -1,7 +1,7 @@
 const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectoryAll('button')
+const possibleChoices = document.querySelectorAll('button')
 let userChoice
 
 possibleChoices.forEach(possiblechoice => possiblechoice.addEventListener('click', (event) => {
