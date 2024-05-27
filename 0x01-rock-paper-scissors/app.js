9e5ae93b3d5866dfae4ad4a6f007dca05cdb9bf8
Dennis Adaku <dennisadaku@ymail.com>
2024-05-27 14:41:16 +0000

    chore: updated function to display computer choice

--- 0x01-rock-paper-scissors/app.js
+++ 0x01-rock-paper-scissors/app.js
@@ -3,14 +3,27 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possiblechoice => 
     possiblechoice.addEventListener('click', (event) => {
     userChoice = event.target.id
     userChoiceDisplay.innerHTML = userChoice
+
+    computerChoice = genComputerChoice()
+    computerChoiceDisplay.innerHTML = computerChoice
 }));
 
-function getComputerChoice() {
+function genComputerChoice() {
     const randomNumber = (Math.floor(Math.random() * possibleChoices.length)) + 1;
-    console.log(randomNumber);
+
+    if (randomNumber === 1) {
+        return 'rock'
+    }
+    if (randomNumber === 2) {
+        return 'paper'
+    }
+    if (randomNumber === 3) {
+        return 'scissors'
+    }
 }
