081f283a468ba5c33a47efd550562d659436fd39
Dennis Adaku <dennisadaku@ymail.com>
2024-05-27 12:18:29 +0000

    fix: app.js error in spelling of 'document'

--- 0x01-rock-paper-scissors/app.js
+++ 0x01-rock-paper-scissors/app.js
@@ -1,7 +1,7 @@
 const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result')
-const possibleChoices = docuemnt.querySelectoryAll('button')
+const possibleChoices = document.querySelectoryAll('button')
 let userChoice
 
 possibleChoices.forEach(possiblechoice => possiblechoice.addEventListener('click', (event) => {
