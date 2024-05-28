fbee2d11c72a4dbdd2f128e375045c90447f5bfa
Dennis Adaku <dennisadaku@ymail.com>
2024-05-28 20:04:47 +0000

    chore: added function 'getResult'

--- 0x03-rock-paper-scissors/app.js
+++ 0x03-rock-paper-scissors/app.js
@@ -5,8 +5,35 @@ let userChoice, computerChoice;
 
 const choices = ['rock', 'paper', 'scissors'];
 
+function getResult(userChoice, computerChoice) {
+    switch (userChoice + computerChoice) {
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            resultDisplay.innerHTML = 'You chose ' + userChoice + 
+            ' and the computer chose ' + computerChoice + ', YOU WIN!';
+            break;
+        
+        case 'rockpaper':
+        case 'paperscissors':
+        case 'scissorsrock':
+            resultDisplay.innerHTML = 'You chose ' + userChoice +
+            ' and the computer chose ' + computerChoice + ', YOU LOSE!';
+            break;
+        
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            resultDisplay.innerHTML = 'You chose ' + userChoice +
+            ' and the computer chose ' + computerChoice + ", IT'S A DRAW!"
+    }
+}
+
 function handleClick(event) {
-    console.log('clicked')
+    userChoice = event.target.innerHTML;
+    randInt = Math.floor(Math.random() * choices.length)
+    computerChoice = choices[randInt]
+    getResult(userChoice, computerChoice)
 };
 
 choices.forEach(choice => {
