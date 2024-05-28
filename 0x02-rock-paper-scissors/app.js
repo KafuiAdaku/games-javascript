4903134f8d0c95d34b840fc8aebda4c557a5d78d
Dennis Adaku <dennisadaku@ymail.com>
2024-05-28 18:30:07 +0000

    refactor: refactored code

--- 0x02-rock-paper-scissors/app.js
+++ 0x02-rock-paper-scissors/app.js
@@ -1,18 +1,18 @@
-let userChoice
-let computerChoice
+const userChoiceDisplay = createElement('h2');
+const computerChoiceDisplay = createElement('h2');
+const resultDisplay = createElement('h2');
 
-const userChoiceDisplay = document.createElement('h2')
-const computerChoiceDisplay = document.createElement('h2')
-const resultDisplay = document.createElement('h2')
-const gameDiv = document.getElementById('game')
+let userChoice, computerChoice;
 
-gameDiv.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
+const gameDiv = getElementById('game');
+gameDiv.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay);
 
-choices = ['rock', 'paper', 'scissors']
+choices = ['rock', 'paper', 'scissors'];
 
-function genComputerChoice() {
-    const rand = Math.floor(Math.random() * choices.length)
-    return choices[rand]
+/***** Helper Functions *****/
+function getComputerChoice() {
+    const randomInt = Math.floor(Math.random() * choices.length)
+    return choices[randomInt]
 }
 
 function getResult() {
@@ -22,32 +22,34 @@ function getResult() {
         case 'scissorspaper':
             resultDisplay.innerHTML = 'YOU WIN!';
             break;
+        
         case 'rockpaper':
         case 'paperscissors':
         case 'scissorsrock':
             resultDisplay.innerHTML = 'YOU LOSE!';
             break;
+        
         case 'rockrock':
         case 'paperpaper':
         case 'scissorsscissors':
-            resultDisplay.innerHTML = "IT'S A DRAW!";
+            resultDisplay.innerHTML = "IT'S A DRAW";
             break;
     }
-
 }
+/***** *****/
 
-const handleClick = (event) =>  {
-    userChoice = event.target.id  // event.target.innerHTML also works just fine
-    userChoiceDisplay.innerHTML = 'User choice: ' + userChoice
-    computerChoice = genComputerChoice()
-    computerChoiceDisplay.innerHTML = 'Computer choice: ' + computerChoice
+function handleClick(event) {
+    userChoice = event.target.id;
+    userChoiceDisplay.innerHTML = 'User choice: ' + userChoice;
+    computerChoice = getComputerChoice()
+    computerChoiceDisplay.innerHTML = computerChoice
+
     getResult()
 }
 
 for (let i = 0; i < choices.length; i++) {
-    const button = document.createElement('button');
+    const button = createElement('button')
     button.id = choices[i]
     button.innerHTML = choices[i]
-    button.addEventListener('click', handleClick)
-    gameDiv.appendChild(button)
+    button.addEventListerner('click', handleClick)
 }
