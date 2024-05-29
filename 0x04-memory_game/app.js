d9a5a5f0ea7b3db4bd314bb8a68c646c0bdc4ac7
Dennis Adaku <dennisadaku@ymail.com>
2024-05-29 13:18:19 +0000

    chore: created array of card which are randomly sorted

---
+++ 0x04-memory_game/app.js
@@ -0,0 +1,54 @@
+const cardArray = [
+    {
+        name: 'lion',
+        img: './images/lion.png'
+    },
+    {
+        name: 'alligator',
+        img: './images/alligator.png'
+    },
+    {
+        name: 'chicken',
+        img: './images/chicken.png'
+    },
+    {
+        name: 'horse',
+        img: './images/horse.png'
+    },
+    {
+        name: 'frog',
+        img: './images/frog.png'
+    },
+    {
+        name: 'elephant',
+        img: './images/elephant.png'
+    },
+    {
+        name: 'lion',
+        img: './images/lion.png'
+    },
+    {
+        name: 'alligator',
+        img: './images/alligator.png'
+    },
+    {
+        name: 'chicken',
+        img: './images/chicken.png'
+    },
+    {
+        name: 'horse',
+        img: './images/horse.png'
+    },
+    {
+        name: 'frog',
+        img: './images/frog.png'
+    },
+    {
+        name: 'elephant',
+        img: './images/elephant.png'
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random())  // randomly sort array
+
+console.log(cardArray)
