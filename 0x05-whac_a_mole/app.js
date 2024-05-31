686bcce776fb7a1f4fd7ea9a90f51b64d2534816
Dennis Adaku <dennisadaku@ymail.com>
2024-05-31 15:47:54 +0000

    edge cases

--- 0x05-whac_a_mole/app.js
+++ 0x05-whac_a_mole/app.js
@@ -9,22 +9,27 @@ let hitPosition = null;
 let timer = null;
 let moveMoleId = null;
 let countDownId = null;
+let gameRunning = false;
 
 startButton.addEventListener('click', () => {
     // Reset game
-    result = 0;
-    timer = 10;
-    score.innerHTML = result;
-    time.innerHTML = timer;
+    if (!gameRunning) {
+        gameRunning = true;
+        result = 0;
+        timer = 10;
+        score.innerHTML = result;
+        time.innerHTML = timer;
 
-    // clear any existing intervals
-    clearInterval(countDownId);
-    clearInterval(moveMoleId);
+        // clear any existing intervals
+        clearInterval(countDownId);
+        clearInterval(moveMoleId);
 
-    moveMole();
-    countDownId = setInterval(countDown, 1000);
+        moveMole();
+        countDownId = setInterval(countDown, 1000);
+    }
 })
 
+// Randomly select squares
 function randomSquare() {
     squares.forEach(square => {
         square.classList.remove('mole')
@@ -35,22 +40,30 @@ function randomSquare() {
     hitPosition = randSquare.id;
 }
 
-squares.forEach(square => {
-    square.addEventListener('mousedown', () => {
-        if (square.id === hitPosition) {
+
+// Event handler for when a mole is clicked
+function handleMouseDown() {
+    if (this.id === hitPosition && gameRunning) {
             result++;
             score.innerHTML = result;
             hitPosition = null;
         }
-    })
+}
+
+
+// Add event listener to each square
+squares.forEach(square => {
+    square.addEventListener('mousedown', handleMouseDown);
 })
 
+
+// Set interval to repeat random selection of squares
 function moveMole() {
     moveMoleId = setInterval(randomSquare, 1000);
 }
 
-// moveMole();
 
+// Countdown functionality
 function countDown() {
     timer--;
     time.innerHTML = timer;
@@ -58,10 +71,11 @@ function countDown() {
     if (timer === 0) {
         clearInterval(countDownId);
         clearInterval(moveMoleId)
+        squares.forEach(square => {
+            square.removeEventListener('mousedown', handleMouseDown )
+        })
+        gameRunning = false;
         alert('GAME OVER! Your final score is ' + result);
 
     }
 }
-
-countDownId =  setInterval(countDown, 1000);
-
