955c96c3f6473113a39c1ab4a3b090ea5cd6eea5
Dennis Adaku <dennisadaku@ymail.com>
2024-06-01 08:39:18 +0000

    chore: creating blocks using class

--- 0x06-breakout/app.js
+++ 0x06-breakout/app.js
@@ -1,12 +1,45 @@
 const grid = document.querySelector('.grid');
+const blockWidth = 100;
+const blockHeight = 20;
 
-function addBlock() {
-    const block = document.createElement('div');
-    block.classList.add('block');
-    block.style.left = '100px';
-    block.style.bottom = '20px';
-    grid.appendChild(block);
+
+// create block class
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
+        this.bottomRight = [xAxis + blockWidth, yAxis];
+        this.topLeft = [xAxis, yAxis + blockHeight];
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+    }
 }
 
-addBlock();
-addBlock();
+
+const blocks = [
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270),
+    new Block(10, 270)
+]  
+// draw all blocks
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.left = blocks[i].bottomLeft[0] + 'px';
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+        grid.appendChild(block);
+    }
+}
+
+addBlocks();
