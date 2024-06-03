5fca9e7584be6ac96cfef5b27855fafe84513e91
Dennis Adaku <dennisadaku@ymail.com>
2024-06-03 11:20:40 +0000

    chore: updated moveFrog function

--- 0x07-frogger/app.js
+++ 0x07-frogger/app.js
@@ -3,21 +3,39 @@ const resultDisplay = document.getElementById('result')
 const startPauseButton = document.getElementById('start-pause-button');
 const squares = document.querySelectorAll('.grid div');
 
+const squaresPerRow = 9;
+
 let frogCurrentIdx = 76;
 
 function moveFrog(event) {
+    // First remove the frog from current position
+    squares[frogCurrentIdx].classList.remove('frog');
+
     switch(event.key) {
         case 'ArrowLeft':
-            console.log('move left');
+            if(frogCurrentIdx % squaresPerRow !== 0) {
+                frogCurrentIdx -= 1
+                console.log('move left');
+            }
             break;
         case 'ArrowRight':
-            console.log('move right');
+            if (frogCurrentIdx % squaresPerRow < squaresPerRow - 1)
+                {
+                    frogCurrentIdx += 1;
+                    console.log('move right');
+                }
             break;
         case 'ArrowDown':
-            console.log('move down');
+            if (frogCurrentIdx + squaresPerRow < squaresPerRow * squaresPerRow) {
+                frogCurrentIdx += squaresPerRow;
+                console.log('move down');
+            }
             break;
         case 'ArrowUp':
-            console.log('move up');
+            if (frogCurrentIdx >= squaresPerRow) {
+                frogCurrentIdx -= squaresPerRow;
+                console.log('move up');
+            }
             break;
     }
     squares[frogCurrentIdx].classList.add('frog');
