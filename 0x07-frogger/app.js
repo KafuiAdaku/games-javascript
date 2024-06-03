ab774298cd3afab3a4343b64360366909c652073
Dennis Adaku <dennisadaku@ymail.com>
2024-06-03 10:47:01 +0000

    chore: partial implementation of frog movement

--- 0x07-frogger/app.js
+++ 0x07-frogger/app.js
@@ -3,22 +3,19 @@ const resultDisplay = document.getElementById('result')
 const startPauseButton = document.getElementById('start-pause-button');
 const squares = document.querySelectorAll('.grid div');
 
-let frogCurrentInd = 76;
+let frogCurrentIdx = 76;
 
 function moveFrog(event) {
     switch(event.key) {
         case 'ArrowLeft':
-            squares[frogCurrentInd].classList.add('frog');
             break;
         case 'ArrowRight':
-            squares[frogCurrentInd].classList.add('frog');
             break;
         case 'ArrowDown':
-            squares[frogCurrentInd].classList.add('frog');
             break;
         case 'ArrowUp':
-            squares[frogCurrentInd].classList.add('frog');
     }
+    squares[frogCurrentIdx].classList.add('frog');
 }
 
 
