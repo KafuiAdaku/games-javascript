17dad70445f851424fda817ebd87602cd7ddf430
Dennis Adaku <dennisadaku@ymail.com>
2024-06-03 10:50:43 +0000

    chore: partial implementation of frog movement

--- 0x07-frogger/app.js
+++ 0x07-frogger/app.js
@@ -8,12 +8,17 @@ let frogCurrentIdx = 76;
 function moveFrog(event) {
     switch(event.key) {
         case 'ArrowLeft':
+            console.log('move left');
             break;
         case 'ArrowRight':
+            console.log('move right');
             break;
         case 'ArrowDown':
+            console.log('move down');
             break;
         case 'ArrowUp':
+            console.log('move up');
+            break;
     }
     squares[frogCurrentIdx].classList.add('frog');
 }
