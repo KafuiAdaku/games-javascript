fe4119d843cb51e4062edbf4d5467d993d3afb29
Dennis Adaku <dennisadaku@ymail.com>
2024-06-04 21:15:48 +0000

    chore: added click event listener to squares

--- 0x08-connect_four/app.js
+++ 0x08-connect_four/app.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div');
+    const resultDisplay = document.querySelector('#result');
+    const currentPlayerDisp = document.querySelector('#current-player')
+    let currentPlayer = 1;
 
+    for (let i = 0; i < squares.length - 7; i++) {
+        squares[i].onclick = () => {
+            alert(`You have clicked square ${i}`);
+        }
+    }
 })
