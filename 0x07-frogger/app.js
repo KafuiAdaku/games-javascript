52b3b67d5adc8248a5b020d439538a443565b4c7
Dennis Adaku <dennisadaku@ymail.com>
2024-06-04 12:55:44 +0000

    fix: fixed improper reference to time-left id

--- 0x07-frogger/app.js
+++ 0x07-frogger/app.js
@@ -1,4 +1,4 @@
-const timeLeftDisplay = document.getElementById('time');
+const timeLeftDisplay = document.getElementById('time-left');
 const resultDisplay = document.getElementById('result')
 const startPauseButton = document.getElementById('start-pause-button');
 const squares = document.querySelectorAll('.grid div');
