7d7aa24de5f5bab4563c6ede81fd77d9703aa942
Dennis Adaku <dennisadaku@ymail.com>
2024-06-05 10:31:46 +0000

    chore: implemented squares selections logic

--- 0x08-connect_four/app.js
+++ 0x08-connect_four/app.js
@@ -3,10 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultDisplay = document.querySelector('#result');
     const currentPlayerDisp = document.querySelector('#current-player')
     let currentPlayer = 1;
+    const sqPerRow = 7;
 
-    for (let i = 0; i < squares.length - 7; i++) {
+    for (let i = 0; i < squares.length - sqPerRow; i++) {
         squares[i].onclick = () => {
-            alert(`You have clicked square ${i}`);
+            if (squares[i + 7].classList.contains('taken')) {
+                if (currentPlayer === 1) {
+                    squares[i].classList.add('player-one');
+                    squares[i].classList.add('taken');
+                    currentPlayer = 2;
+                } else if (currentPlayer ===2) {
+                    squares[i].classList.add('player-two');
+                    squares[i].classList.add('taken');
+                    currentPlayer = 1;
+                } else {alert("Can't go here");}
+                currentPlayerDisp.innerHTML = currentPlayer;
+            } 
         }
     }
 })
